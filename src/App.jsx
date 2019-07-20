0980f2e20a0c148a1d42b5395edc46487203664e
KAB <47466350+KwasiBell@users.noreply.github.com>
2019-07-20 12:16:48 -0400

    added card/wrapper js and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from "react";
 import Card from "./components/Card/Card";
 import './App.css';
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 import cards from "./cards.json";
 
 
 
 class App extends React.Component {
  state = {
-   message: 'Click any image to begin',
+   message: 'Click on any Athlete image to begin',
    score: 0,
    cardsArray: cards,
    clickedArray: [],
@@ -53,108 +55,62 @@ class App extends React.Component {
 
   }
 
-  render() {
 
-    return(
-      <>
-      <h1>{this.state.message}</h1>
-      <p>
-      <span>Score: {this.state.score}</span>{" "}<span>High score: {this.state.highScore}</span>
-      </p>
-      {this.state.cardsArray.map(eachObject => {
-        return <div
-           key={eachObject.id}
-           onClick={() => this.handleClick(eachObject.id)}
-          >
-            <h1>{eachObject.id}</h1>
-            <p>{eachObject.name}</p>
-            <img src= {eachObject.image} width="250" height="250" alt=""/>
-        </div>
-      })}
-       </>
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>Athlete Clicky Game</Header>
+        {this.state.cardsArray.map(card => (
+          <Card
+            clickCount={this.handleClick}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
     )
-  }
+    };
+  // render() {
+
+  //   return(
+  //     <Wrapper>
+  //     <h1>{this.state.message}</h1>
+  //     <p>
+  //     <span>Score: {this.state.score}</span>{" "}<span>High score: {this.state.highScore}</span>
+  //     </p>
+  //     {this.state.cardsArray.map(eachObject => {
+  //       return <div
+  //          key={eachObject.id}
+  //          onClick={() => this.handleClick(eachObject.id)}
+  //         >
+  //           <h1>{eachObject.id}</h1>
+  //           <p>{eachObject.name}</p>
+  //           <img src= {eachObject.image} width="250" height="250" alt=""/>
+  //       </div>
+  //     })}
+  //      </Wrapper>
+  //   )
+  // }
 }
 
+//  render() {
+//   return (
+//     <Wrapper>
+//       <Header score={this.state.score} highscore={this.state.highscore}>Athlete Clicky Game</Header>
+//       {this.state.cards.map(card => (
+//         <Card
+//           clickCount={this.clickCount}
+//           id={card.id}
+//           key={card.id}
+//           image={card.image}
+//         />
+//       ))}
+//     </Wrapper>
+//   )
+//   };
+
+
+
+
 export default App;
-
-/*
-
-// var images = [
-//   {
-//     "id": 1,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 2,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 3,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 4,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 5,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 6,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 7,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 8,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 9,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 10,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 11,
-//     url: logo,
-//     "count": 0
-
-//   },
-//   {
-//     "id": 12,
-//     url: logo,
-//     "count": 0
-
-//   },
-// ]
-
-
-
-*/
